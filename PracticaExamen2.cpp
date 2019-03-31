15557454e08669d847c44829fe4cd9d7865ffa8e
Lucas Stirpe <49053857+lucasnahuels@users.noreply.github.com>
2019-03-31 03:27:56 -0300

    Add files via upload

---
+++ PracticaExamen2.cpp
@@ -0,0 +1,215 @@
+#include <stdio.h>
+#include <string.h>
+#include <iostream>
+#include <stdlib.h>
+
+using namespace std;
+
+class Voto{
+    public:
+       char nombre [30];
+       char sino [3];
+       int pos;
+       int neg;
+       Voto * sig;
+
+       Voto(){
+       nombre[0]=0;
+       sino[0]=0;
+       pos=0;
+       neg=0;
+       sig=0;
+       }
+
+       ~Voto(){
+       cout <<"Eliminando candidato: " <<nombre <<endl;
+       }
+};
+//===============================================================================================================================
+class listaVotos{
+    private:
+       Voto * inicio;
+
+    public:
+       listaVotos(){
+       inicio=0;
+       }
+
+       ~listaVotos(){
+       while(inicio) borrar(inicio);
+       cout <<"Eliminando lista..." <<endl;
+       }
+//-------------------------------------------------------------------------------------------------------------------------------
+    void agregar (char * nombre, char * sino){
+
+       Voto * nuevo = new Voto;
+       strcpy(nuevo->nombre, nombre);
+       strcpy(nuevo->sino, sino);
+
+       strupr(sino);
+       if(strcmp(sino,"SI")==0) nuevo->pos++;
+       if(strcmp(sino,"NO")==0) nuevo->neg++;
+
+       if(inicio==0){
+       inicio=nuevo;
+       return;
+       }
+
+       Voto * aux = inicio;
+       while(aux->sig!=0){
+       aux=aux->sig;
+       }
+       aux->sig = nuevo;
+   }
+//-------------------------------------------------------------------------------------------------------------------------------
+    int contar (){
+
+       int cont=0;
+       Voto * aux = inicio;
+
+       while(aux!=0){
+        cont++;
+        aux=aux->sig;
+       }
+       return cont;
+    }
+//-------------------------------------------------------------------------------------------------------------------------------
+    void mirar (){
+       if(inicio==0){
+        cout <<"Lista vacia" <<endl;
+        return;
+       }
+
+       Voto * aux = inicio;
+       while(aux!=0){
+        cout<<"Candidato: "<<aux->nombre <<"  Votos Positivos: " <<aux->pos <<"  Votos Negativos: " <<aux->neg <<endl;
+        aux=aux->sig;
+       }
+    }
+//-------------------------------------------------------------------------------------------------------------------------------
+    void desenganchar (Voto * cual){
+       if(cual == inicio){
+       inicio = inicio->sig;
+       cual->sig=0;
+       return;
+       }
+       Voto * aux = inicio;
+         while(aux->sig != cual){ // Busco al anterior al que tengo que sacar
+             aux = aux->sig; //El bucle corta cuando en aux me queda la dirección del anterior
+         }
+         aux->sig = cual->sig;
+         cual->sig=0;
+        }
+//-------------------------------------------------------------------------------------------------------------------------------
+    void borrar(Voto * quien){
+       if(inicio==0) return;
+       if(quien==0)return;
+
+       desenganchar(quien);
+       delete quien;
+    }
+//-------------------------------------------------------------------------------------------------------------------------------
+    void * buscar(char * quien){
+       if(inicio==0) return 0;
+       Voto * aux = inicio;
+
+       while(aux){
+         if(strcmp(aux->nombre,quien)==0) return aux;
+         aux=aux->sig;
+        }
+       return 0; // Si no lo encontró
+    }
+//-------------------------------------------------------------------------------------------------------------------------------
+    void * cuentaVoto(char * quien, char * vot){
+       Voto * aux = inicio;
+
+       while(aux){
+        if(strcmp(aux->nombre,quien)==0){
+        strupr(vot);
+        if(strcmp(vot,"SI")==0) aux->pos++;
+        if(strcmp(vot,"NO")==0) aux->neg++;
+        aux=aux->sig;
+       }
+       return 0; // Si no lo encontró
+     }
+    }
+//-------------------------------------------------------------------------------------------------------------------------------
+        Voto * buscarMinimo (void){
+          Voto * minimo = inicio;
+          Voto * aux = inicio->sig;
+
+          while(aux!=0){
+            if(aux->neg>minimo->neg){
+                minimo = aux;
+            }
+            aux = aux->sig;
+          }
+          return minimo;
+        }
+//-------------------------------------------------------------------------------------------------------------------------------
+        Voto * buscarMax (void){
+          Voto * maxi = inicio;
+          Voto * aux = inicio->sig;
+
+          while(aux!=0){
+            if(aux->pos>maxi->pos){
+                maxi = aux;
+            }
+            aux = aux->sig;
+          }
+          return maxi;
+        }
+//-------------------------------------------------------------------------------------------------------------------------------
+    int buscaPor(){
+       Voto * aux = inicio;
+       float cont=0;
+
+       while(aux!=0){
+        if(aux->neg>0){
+         cont++;
+        }
+        aux=aux->sig;
+       }
+       cont=(cont/contar())*100;
+
+       return cont;
+     }
+};
+//===============================================================================================================================
+
+int main(){
+
+ listaVotos * lv = new listaVotos;
+ char nom[30];
+ char vot[3];
+
+ cout<<"Ingrese el candidato e indique SI o NO, ingrese Fin para salir \n" <<endl;
+ cin.getline(nom,30);
+ strupr(nom);
+
+ while(strcmp(nom,"FIN")!=0){
+
+    if(lv->buscar(nom)==0){
+    cin.getline(vot,3);
+    lv->agregar(nom,vot);
+    cout<<"Ingrese el candidato e indique SI o NO, ingrese Fin para salir \n" <<endl;
+    cin.getline(nom,30);
+    strupr(nom);
+    }
+    else{
+       cin.getline(vot,3);
+       lv->cuentaVoto(nom,vot);
+       cout<<"Ingrese el candidato e indique SI o NO, ingrese Fin para salir \n" <<endl;
+       cin.getline(nom,30);
+       strupr(nom);
+     }
+ }
+
+ system("cls");
+
+ lv->mirar();
+ cout<<"\n Hay " <<lv->contar() <<" Candidatos" <<endl;
+ cout<<"\n El idolo popular es: " <<lv->buscarMax()->nombre <<endl;
+ cout<<"\n El Salvavidas de plomo es: " <<lv->buscarMinimo()->nombre <<endl;
+ cout<<"\n El porcentaje de gente con votos negativos es: " <<lv->buscaPor() <<" porciento";
+}
---
+++ juegodepuntos.cpp
@@ -0,0 +1,205 @@
+#include <iostream>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+using namespace std;
+
+class JUGADOR{
+	public:
+		char NOM[20];
+		int PUNTOS;
+		JUGADOR * SIG;
+		JUGADOR(char[] , int);
+		~JUGADOR();
+};
+
+
+JUGADOR::JUGADOR(char S[], int P){
+	strcpy(NOM, S);
+	PUNTOS=P;	
+}
+
+JUGADOR::~JUGADOR(){
+	cout<<"\tA "<<NOM<<" LO ECHARON DEL JUEGO"<<endl;
+}
+
+
+class EXTENSION{
+	JUGADOR * PRIMERO;
+	public:
+		int ANALIZAR(char[] , int );
+		void ELIMINAR(JUGADOR *);
+		int BUSCAR(char []);
+		void AGREGAR(JUGADOR *);
+		EXTENSION();
+		~EXTENSION();
+		
+};
+
+int EXTENSION::ANALIZAR(char S[], int P){
+	JUGADOR * BUSCADOR, * ELIM;
+	BUSCADOR = PRIMERO;
+	int I=0;
+	cout<<"\t\tEL JUGADOR "<<S<<" SACO UN "<<P<<endl;
+	
+	if(P==8){
+		if(!BUSCAR(S)){
+			JUGADOR * N;
+			N = new JUGADOR(S , P);
+			AGREGAR(N);
+			cout<<"\t\t\t "<<N->NOM<<" SE UNIO AL JUEGO"<<endl;
+			return 2;
+		}
+	}
+	BUSCADOR=PRIMERO;
+	
+	if(P==9){
+		while(BUSCADOR){
+			if(strcmp(BUSCADOR->NOM , S)==0){
+				BUSCADOR->PUNTOS=BUSCADOR->PUNTOS - 5;
+				cout<<"\t\t\t\t "<<BUSCADOR->NOM<<" PERDIO 5 PUNTOS AHORA TIENE "<<BUSCADOR->PUNTOS<<" PUNTOS!!!!"<<endl;
+				return 5;
+			}
+			BUSCADOR=BUSCADOR->SIG;
+		}
+			
+	}
+	
+	BUSCADOR=PRIMERO;
+	while(BUSCADOR){
+		if(strcmp(BUSCADOR->NOM , S)==0 && P!=9 && P!=1 && P!=0){
+			BUSCADOR->PUNTOS=BUSCADOR->PUNTOS + P ;
+			cout<<"\t\t\t\t\tAHORA "<<BUSCADOR->NOM<<" TIENE "<<BUSCADOR->PUNTOS<<" PUNTOS!!!!"<<endl;
+			if(BUSCADOR->PUNTOS >= 100){
+				return 4;
+			
+			}
+			return 1;
+		}
+		if(strcmp(BUSCADOR->NOM , S)==0 && P<=1)
+			ELIM=BUSCADOR;
+		BUSCADOR=BUSCADOR->SIG;
+	}
+	BUSCADOR=PRIMERO;
+	if(P<=1){
+		while(BUSCADOR){
+			if(strcmp(BUSCADOR->NOM , S)==0){
+				cout<<"\t\t\t\t\t\tSE ELIMINARA A "<<ELIM->NOM<<endl;
+				ELIMINAR(ELIM);
+			}
+			BUSCADOR=BUSCADOR->SIG;
+		}		
+	}
+
+	return 0;
+	
+}
+
+void EXTENSION::AGREGAR(JUGADOR * N){
+	JUGADOR * B;
+	B=PRIMERO;
+	
+	if(!PRIMERO){
+		PRIMERO=N;
+		N->SIG=NULL;
+		return;
+	}
+	
+	while(B){
+		if(!B->SIG){
+			B->SIG=N;
+			N->SIG=NULL;
+		}
+		B=B->SIG;
+	} 
+}
+
+int EXTENSION::BUSCAR(char S[]){
+	JUGADOR * B;
+	B=PRIMERO;
+
+	
+	while(B){
+		if(strcmp(B->NOM, S)==0)
+			return 1;
+		B=B->SIG;
+	}
+	return 0;
+}
+
+void EXTENSION::ELIMINAR(JUGADOR * PERDIO){
+	JUGADOR * BUSC;
+	BUSC=PRIMERO;
+	
+	if(PRIMERO==PERDIO){
+		PRIMERO=PERDIO->SIG;
+		delete PERDIO;
+		return;
+	}
+	
+	while(BUSC->SIG){
+		if(BUSC->SIG==PERDIO){
+			BUSC->SIG=PERDIO->SIG;
+			delete PERDIO;
+			return;
+		}
+		BUSC=BUSC->SIG;
+	}
+	
+}
+
+EXTENSION::EXTENSION(){
+	PRIMERO=NULL;
+}
+
+EXTENSION::~EXTENSION(){
+	cout<<"ACA MUEREN TODOS";
+}
+
+char * GENERARNOM();
+
+int main(){
+	EXTENSION E;
+	int F=1 , P, C;
+	char NOM[20];
+	int I=0;
+	
+	while(F==1){
+		P=rand()%10;
+		strcpy( NOM, GENERARNOM());
+		C = E.ANALIZAR(NOM, P);
+		
+		//cout<<NOM<<" SACO UN "<<P<<endl;
+	//	cout<<endl<<C<<endl;//MUESTRA LA DESICION DEL E.ANALIZAR;
+		switch(C){
+			case 0 : break;			//cout<<"NADIE SE UNIO AL JUEGO... "<<NOM<<" TENIA SOLO "<<P<<" PUNTOS";
+			case 1 : break;			//cout<<"A "<<NOM<<" SE LE AGREGARON "<<P<<" PUNTOS!!!"<<endl;	
+			case 2 : break;			//cout<<NOM<<" SE A UNIDO AL JUEGO!!!"<<endl;break;	
+			case 3 : break;			//cout<<"EL JUGADOR "<<NOM<<" PERDIO, FUE RETIRADO DEL JUEGO"<<endl;
+			case 4 : F=0;   break; // UNO GANO;
+			case 5 : break; //PERDIO 5 PUNHTOS;
+				
+				
+		}// TODO ESTO SE PUEDE REEMPLAZAR POR UN IF(C==4) PERO CON EL SWITCH TE MUESTRA EL CAMINO QUE TOMA;
+		if(I==30){
+			I=0;
+			getchar();
+		}
+		else
+		 	I++;
+		
+	}
+	
+	cout<<"FIN DEL PROGRAMA";
+	
+	
+}
+
+
+char * GENERARNOM(){
+	static char NOM[][20]= {"PEPE","LOLA","DANIELA","LAURA",
+							"RAMIRO","ROMAN","MANUEL","ELIAS",
+							"ANSELMO","MONICA","CAMILA","LUNA"};
+	return NOM[rand()%12];
+}
