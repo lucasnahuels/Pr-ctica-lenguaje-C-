4e17bd5c4259a27b3a43b5b8a6e799141ad4a287
Lucas Stirpe <49053857+lucasnahuels@users.noreply.github.com>
2021-04-09 16:15:58 -0300

    Update juegodepuntos.cpp

--- juegodepuntos.cpp
+++ juegodepuntos.cpp
@@ -15,12 +15,14 @@ class JUGADOR{
 };
 
 
-JUGADOR::JUGADOR(char S[], int P){
+JUGADOR::JUGADOR(char S[], int P)
+{
 	strcpy(NOM, S);
 	PUNTOS=P;	
 }
 
-JUGADOR::~JUGADOR(){
+JUGADOR::~JUGADOR()
+{
 	cout<<"\tA "<<NOM<<" LO ECHARON DEL JUEGO"<<endl;
 }
 
@@ -37,14 +39,17 @@ class EXTENSION{
 		
 };
 
-int EXTENSION::ANALIZAR(char S[], int P){
+int EXTENSION::ANALIZAR(char S[], int P)
+{
 	JUGADOR * BUSCADOR, * ELIM;
 	BUSCADOR = PRIMERO;
 	int I=0;
 	cout<<"\t\tEL JUGADOR "<<S<<" SACO UN "<<P<<endl;
 	
-	if(P==8){
-		if(!BUSCAR(S)){
+	if(P==8)
+	{
+		if(!BUSCAR(S))
+		{
 			JUGADOR * N;
 			N = new JUGADOR(S , P);
 			AGREGAR(N);
@@ -54,9 +59,12 @@ int EXTENSION::ANALIZAR(char S[], int P){
 	}
 	BUSCADOR=PRIMERO;
 	
-	if(P==9){
-		while(BUSCADOR){
-			if(strcmp(BUSCADOR->NOM , S)==0){
+	if(P==9)
+	{
+		while(BUSCADOR)
+		{
+			if(strcmp(BUSCADOR->NOM , S)==0)
+			{
 				BUSCADOR->PUNTOS=BUSCADOR->PUNTOS - 5;
 				cout<<"\t\t\t\t "<<BUSCADOR->NOM<<" PERDIO 5 PUNTOS AHORA TIENE "<<BUSCADOR->PUNTOS<<" PUNTOS!!!!"<<endl;
 				return 5;
@@ -67,13 +75,15 @@ int EXTENSION::ANALIZAR(char S[], int P){
 	}
 	
 	BUSCADOR=PRIMERO;
-	while(BUSCADOR){
-		if(strcmp(BUSCADOR->NOM , S)==0 && P!=9 && P!=1 && P!=0){
+	while(BUSCADOR)
+	{
+		if(strcmp(BUSCADOR->NOM , S)==0 && P!=9 && P!=1 && P!=0)
+		{
 			BUSCADOR->PUNTOS=BUSCADOR->PUNTOS + P ;
 			cout<<"\t\t\t\t\tAHORA "<<BUSCADOR->NOM<<" TIENE "<<BUSCADOR->PUNTOS<<" PUNTOS!!!!"<<endl;
-			if(BUSCADOR->PUNTOS >= 100){
+			if(BUSCADOR->PUNTOS >= 100)
+			{
 				return 4;
-			
 			}
 			return 1;
 		}
@@ -82,9 +92,12 @@ int EXTENSION::ANALIZAR(char S[], int P){
 		BUSCADOR=BUSCADOR->SIG;
 	}
 	BUSCADOR=PRIMERO;
-	if(P<=1){
-		while(BUSCADOR){
-			if(strcmp(BUSCADOR->NOM , S)==0){
+	if(P<=1)
+	{
+		while(BUSCADOR)
+		{
+			if(strcmp(BUSCADOR->NOM , S)==0)
+			{
 				cout<<"\t\t\t\t\t\tSE ELIMINARA A "<<ELIM->NOM<<endl;
 				ELIMINAR(ELIM);
 			}
@@ -96,18 +109,22 @@ int EXTENSION::ANALIZAR(char S[], int P){
 	
 }
 
-void EXTENSION::AGREGAR(JUGADOR * N){
+void EXTENSION::AGREGAR(JUGADOR * N)
+{
 	JUGADOR * B;
 	B=PRIMERO;
 	
-	if(!PRIMERO){
+	if(!PRIMERO)
+	{
 		PRIMERO=N;
 		N->SIG=NULL;
 		return;
 	}
 	
-	while(B){
-		if(!B->SIG){
+	while(B)
+	{
+		if(!B->SIG)
+		{
 			B->SIG=N;
 			N->SIG=NULL;
 		}
@@ -115,10 +132,10 @@ void EXTENSION::AGREGAR(JUGADOR * N){
 	} 
 }
 
-int EXTENSION::BUSCAR(char S[]){
+int EXTENSION::BUSCAR(char S[])
+{
 	JUGADOR * B;
 	B=PRIMERO;
-
 	
 	while(B){
 		if(strcmp(B->NOM, S)==0)
@@ -128,18 +145,21 @@ int EXTENSION::BUSCAR(char S[]){
 	return 0;
 }
 
-void EXTENSION::ELIMINAR(JUGADOR * PERDIO){
+void EXTENSION::ELIMINAR(JUGADOR * PERDIO)
+{
 	JUGADOR * BUSC;
 	BUSC=PRIMERO;
 	
-	if(PRIMERO==PERDIO){
+	if(PRIMERO==PERDIO)
+	{
 		PRIMERO=PERDIO->SIG;
 		delete PERDIO;
 		return;
 	}
 	
 	while(BUSC->SIG){
-		if(BUSC->SIG==PERDIO){
+		if(BUSC->SIG==PERDIO)
+		{
 			BUSC->SIG=PERDIO->SIG;
 			delete PERDIO;
 			return;
@@ -149,40 +169,46 @@ void EXTENSION::ELIMINAR(JUGADOR * PERDIO){
 	
 }
 
-EXTENSION::EXTENSION(){
+EXTENSION::EXTENSION()
+{
 	PRIMERO=NULL;
 }
 
-EXTENSION::~EXTENSION(){
+EXTENSION::~EXTENSION()
+{
 	cout<<"ACA MUEREN TODOS";
 }
 
 char * GENERARNOM();
 
-int main(){
+int main()
+{
 	EXTENSION E;
 	int F=1 , P, C;
 	char NOM[20];
 	int I=0;
 	
-	while(F==1){
+	while(F==1)
+	{
 		P=rand()%10;
 		strcpy( NOM, GENERARNOM());
 		C = E.ANALIZAR(NOM, P);
 		
 		//cout<<NOM<<" SACO UN "<<P<<endl;
-	//	cout<<endl<<C<<endl;//MUESTRA LA DESICION DEL E.ANALIZAR;
-		switch(C){
-			case 0 : break;			//cout<<"NADIE SE UNIO AL JUEGO... "<<NOM<<" TENIA SOLO "<<P<<" PUNTOS";
-			case 1 : break;			//cout<<"A "<<NOM<<" SE LE AGREGARON "<<P<<" PUNTOS!!!"<<endl;	
-			case 2 : break;			//cout<<NOM<<" SE A UNIDO AL JUEGO!!!"<<endl;break;	
-			case 3 : break;			//cout<<"EL JUGADOR "<<NOM<<" PERDIO, FUE RETIRADO DEL JUEGO"<<endl;
+		//cout<<endl<<C<<endl;//MUESTRA LA DESICION DEL E.ANALIZAR;
+		switch(C)
+		{
+			case 0 : break;	//cout<<"NADIE SE UNIO AL JUEGO... "<<NOM<<" TENIA SOLO "<<P<<" PUNTOS";
+			case 1 : break;	//cout<<"A "<<NOM<<" SE LE AGREGARON "<<P<<" PUNTOS!!!"<<endl;	
+			case 2 : break;	//cout<<NOM<<" SE A UNIDO AL JUEGO!!!"<<endl;break;	
+			case 3 : break;	//cout<<"EL JUGADOR "<<NOM<<" PERDIO, FUE RETIRADO DEL JUEGO"<<endl;
 			case 4 : F=0;   break; // UNO GANO;
 			case 5 : break; //PERDIO 5 PUNHTOS;
 				
 				
 		}// TODO ESTO SE PUEDE REEMPLAZAR POR UN IF(C==4) PERO CON EL SWITCH TE MUESTRA EL CAMINO QUE TOMA;
-		if(I==30){
+		if(I==30)
+		{
 			I=0;
 			getchar();
 		}
@@ -192,12 +218,11 @@ int main(){
 	}
 	
 	cout<<"FIN DEL PROGRAMA";
-	
-	
 }
 
 
-char * GENERARNOM(){
+char * GENERARNOM()
+{
 	static char NOM[][20]= {"PEPE","LOLA","DANIELA","LAURA",
 							"RAMIRO","ROMAN","MANUEL","ELIAS",
 							"ANSELMO","MONICA","CAMILA","LUNA"};
